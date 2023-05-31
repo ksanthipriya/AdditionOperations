fd6179b91ae6cd93b39a3e51082b4a34832815a5
ksanthipriya <santhipriyako96@gmail.com>
2023-05-31 02:28:13 -0700

    this is operations project

---
+++ AdditionOperations/src/com/sathya/Addition.java
@@ -0,0 +1,11 @@
+package com.sathya;
+
+public class Addition {
+       public static void main(String[] args) {
+    	   int num1=10;
+    	   int num2=20;
+    	   int result;
+    	   result=num1+num2;
+    	   System.out.println("Addition of two numbers is"+" "+result);
+       }
+}
